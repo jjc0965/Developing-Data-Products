f5bb9e3a448cdbbd2dc1bb568dadc6b78187cf07
John <jcomo@metrohealth.org>
2015-12-21 16:26:35 -0500

    Sending files to github

---
+++ server.R
@@ -0,0 +1,18 @@
+library(shiny)
+library(datasets)
+data("esoph")
+
+esophRisk<-function(age, alcohol, tobacco) {
+  row<-which((esoph$agegp==age)&(esoph$alcgp==alcohol)&(esoph$tobgp==tobacco))
+  esophRisk<-round(100*esoph[row, 4]/(esoph[row, 4]+esoph[row, 5]), 1)
+  paste0(as.character(esophRisk),"%")
+}
+
+shinyServer(
+  function(input, output) {
+    output$oid1<-renderPrint({input$`age range`})
+    output$oid2<-renderPrint({input$`alcohol intake`})
+    output$oid3<-renderPrint({input$`tobacco intake`})
+    output$prediction<-renderPrint({esophRisk(input$`age range`, input$`alcohol intake`, input$`tobacco intake`)})
+  }
+)
---
+++ ui.R
@@ -0,0 +1,35 @@
+library(datasets)
+library(shiny)
+
+shinyUI(pageWithSidebar(
+  headerPanel("Esophageal cancer risk"),
+  sidebarPanel(
+    h4('Data from a case-control study of esophageal cancer in Ille-et-Vilaine, France'),
+    checkboxGroupInput("age range", "Age range:",
+                       c("25-34"="25-34",
+                         "35-44"="35-44",
+                         "45-54"="45-54",
+                         "55-64"="55-64",
+                         "65-74"="65-74",
+                         "75+"="75+"), selected="25-34"),
+    checkboxGroupInput("alcohol intake", "Alcohol intake:",
+                       c("0-39g/day"="0-39g/day",
+                         "40-79"="40-79",
+                         "80-119"="80-119",
+                         "120+"="120+"), selected="0-39g/day"),
+    checkboxGroupInput("tobacco intake", "Tobacco intake:",
+                       c("0-9g/day"="0-9g/day",
+                         "10-19"="10-19",
+                         "20-29"="20-29",
+                         "30+"="30+"), selected="0-9g/day")
+  ),
+  mainPanel(
+    h3('Results of Prediction'),
+    h4('You entered'),
+    verbatimTextOutput("oid1"),
+    verbatimTextOutput("oid2"),
+    verbatimTextOutput("oid3"),
+    h4('Which resulted in a risk for esophageal cancer of '),
+    verbatimTextOutput("prediction")
+  )
+))
